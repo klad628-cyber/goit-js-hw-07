d5d7bf154089d523c52f9cc462f07ffbbebb523d
klad628-cyber <klad628@gmail.com>
2026-07-03 12:22:42 +0300

    feat: complete js homework tasks

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+  const email = formData.get('email').trim();
+  const password = formData.get('password').trim();
+
+  if (email === '' || password === '') {
+    alert('Please fill in all fields');
+    return;
+  }
+
+  console.log({ email, password });
+  event.currentTarget.reset();
+});
